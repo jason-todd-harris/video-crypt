c9fe4741f052de47c7bbb80f8ee97c4143c5a984
jason-todd-harris <onowahoo@gmail.com>
2015-12-16 15:28:15 -0500

    first commit

---
+++ crypt-video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  crypt-video
+//
+//  Created by JASON HARRIS on 12/16/15.
+//  Copyright © 2015 jason harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ crypt-video/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  crypt-video
+//
+//  Created by JASON HARRIS on 12/16/15.
+//  Copyright © 2015 jason harris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
